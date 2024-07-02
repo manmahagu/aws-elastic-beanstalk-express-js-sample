e26927dd58c8ef977be2d5edbf2c906fbbc8eb00
Angkarn <angkarns@checkpoint.com>
2024-07-02 12:56:22 +0700

    Rev2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World - Angkarn Here!'));
+app.get('/', (req, res) => res.send('Hello World - Angkarn Here 555!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
